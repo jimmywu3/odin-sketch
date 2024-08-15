034d0bb810c142cbd9b6274141511d3eeef6128c
Jimmy Wu <jimmywu063@gmail.com>
2024-08-15 12:53:28 -0700

    Current version of javascript.ks makes it overflow from the border, next version on it maybe make more container divs for each row and populate each row div with element divs

--- javascript.js
+++ javascript.js
@@ -1,31 +1,41 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#resize");
+const INITAL_DIMENSION = 16;
 
-for(let i = 1; i <= 256; ++i){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style["background-color"] = "red";
-    square.style["width"] = "10px";
-    square.style["height"] = "10px";
-    square.style["margin"] = "1px"; 
-    square.addEventListener("mouseover", () => {
-        square.style["background-color"] = "blue";
-    });
-    container.appendChild(square);
-    if(i % 16 == 0){
-        const lineBreak = document.createElement("div");
-        lineBreak.classList.add("break");
-        lineBreak.style["flex-basis"] = "100%";
-        lineBreak.style["width"] = "0";
-        container.appendChild(lineBreak);
-    }
+let createCanvas = (dimension) => {
+    for(let i = 1; i <= Math.pow(dimension, 2); ++i){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style["background-color"] = "red";
+        square.style["flex"] = "1 1 0px"
+        square.style["height"] = `calc(960px/${dimension} - 1px)`;
+        square.style["margin"] = "1px"; 
+        square.addEventListener("mouseover", () => {
+            square.style["background-color"] = "blue";
+        });
+        container.appendChild(square);
+        if(i % dimension == 0){
+            const lineBreak = document.createElement("div");
+            lineBreak.classList.add("break");
+            lineBreak.style["flex-basis"] = "100%";
+            lineBreak.style["width"] = "0";
+            lineBreak.style["height"] = "0";
+            container.appendChild(lineBreak);
+        }
+    }    
 }
 
+
 let resize = () => {
-    let size = prompt("Enter new grid size (max: 100):")
+    let size = prompt("Enter new grid size (max: 100):");
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    createCanvas(size);
     
 };
 
+createCanvas(INITAL_DIMENSION);
 btn.addEventListener("click", resize);
 
 
