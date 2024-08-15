6954e9dc02bc4daf1302e296c2a9e3df2a42ffa5
Jimmy Wu <jimmywu063@gmail.com>
2024-08-15 10:54:12 -0700

    Created button in index.html, Added js to prompt user for a number when clicking button, centered elements using css

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#resize");
 
 for(let i = 1; i <= 256; ++i){
     const square = document.createElement("div");
     square.classList.add("square");
     square.style["background-color"] = "red";
-    square.style["width"] = "5px";
-    square.style["height"] = "5px";
+    square.style["width"] = "10px";
+    square.style["height"] = "10px";
     square.style["margin"] = "1px"; 
+    square.addEventListener("mouseover", () => {
+        square.style["background-color"] = "blue";
+    });
     container.appendChild(square);
     if(i % 16 == 0){
         const lineBreak = document.createElement("div");
@@ -16,6 +20,13 @@ for(let i = 1; i <= 256; ++i){
         container.appendChild(lineBreak);
     }
 }
+
+let resize = () => {
+    let size = prompt("Enter new grid size (max: 100):")
+    
+};
+
+btn.addEventListener("click", resize);
 
 
 
