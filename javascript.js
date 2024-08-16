a488acbdd4a4bd3384a6e38ca0a89789f459523b
Jimmy Wu <jimmywu063@gmail.com>
2024-08-16 15:23:22 -0700

    Started rewriting javascript.js to create row divs

--- javascript.js
+++ javascript.js
@@ -3,26 +3,9 @@ const btn = document.querySelector("#resize");
 const INITAL_DIMENSION = 16;
 
 let createCanvas = (dimension) => {
-    for(let i = 1; i <= Math.pow(dimension, 2); ++i){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.style["background-color"] = "red";
-        square.style["flex"] = "1 1 0px"
-        square.style["height"] = `calc(960px/${dimension} - 1px)`;
-        square.style["margin"] = "1px"; 
-        square.addEventListener("mouseover", () => {
-            square.style["background-color"] = "blue";
-        });
-        container.appendChild(square);
-        if(i % dimension == 0){
-            const lineBreak = document.createElement("div");
-            lineBreak.classList.add("break");
-            lineBreak.style["flex-basis"] = "100%";
-            lineBreak.style["width"] = "0";
-            lineBreak.style["height"] = "0";
-            container.appendChild(lineBreak);
-        }
-    }    
+    for(let i = 0; i < dimension; ++i){
+        const rowDiv = document.createElement("div");
+    }
 }
 
 
