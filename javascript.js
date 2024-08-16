4ff4e2cc8629ef4cf33decee6ce49b1c440dfe06
Jimmy Wu <jimmywu063@gmail.com>
2024-08-16 15:59:55 -0700

    Finished creating row divs and putting element divs in them, works much better now, Fixed overflow issue of previous version

--- javascript.js
+++ javascript.js
@@ -5,6 +5,21 @@ const INITAL_DIMENSION = 16;
 let createCanvas = (dimension) => {
     for(let i = 0; i < dimension; ++i){
         const rowDiv = document.createElement("div");
+        rowDiv.style["display"] = "flex";
+        rowDiv.style["flex-direction"] = "column";
+        rowDiv.style["flex"] = "1 1 0px";
+        rowDiv.style["gap"] = "1px";
+        rowDiv.classList.add("row");
+        for(let j = 0; j < dimension; ++j){
+            const square = document.createElement("div");
+            square.style["background-color"] = "red";
+            square.style["flex"] = "1 1 0px";
+            square.addEventListener("mouseover", () => {
+                square.style["background-color"] = "blue";
+            });
+            rowDiv.appendChild(square);
+        }
+        container.appendChild(rowDiv);
     }
 }
 
